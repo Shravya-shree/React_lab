9bcea4fdac3b1ca6485ebcf18c962d46a2c636de
Shravya-shree <sumanthpoojary2329@gmail.com>
2026-04-07 12:44:45 +0530

    lab9

---
+++ Contact9.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Contact()
+{
+return(
+<div>
+    <h2>Contact Page</h2>
+    <p><b>Phone No.:</b>94648910</p>
+    <p><b>Email:</b>abc@gmail.com</p>
+</div>
+);
+}
+export default Contact;
