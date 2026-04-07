0e6aa1a184ebc8498b17f74c3d4ad6a89dd9e6e0
Shravya-shree <sumanthpoojary2329@gmail.com>
2026-04-07 12:46:37 +0530

    lab9

---
+++ About9.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function About()
+{
+return(
+    <div>
+<h2>About Page</h2>
+<p><b>Name:</b>Shravya</p>
+<p><b>Age:</b>20</p>
+<p><b>Department:</b>Computer Science and Engineering</p>
+<p><b>College:</b>SMVITM Bantakal</p>
+</div>
+);
+}
+export default About;
